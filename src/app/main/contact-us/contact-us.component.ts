70f4159616c56f29faf13f8bb9473d41a4af19e6
Davit Buloyan <davobul@gmail.com>
2020-08-18 18:49:06 +0400

    Done Form logical part steal work on send php

--- src/app/main/contact-us/contact-us.component.ts
+++ src/app/main/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { Http } from "@angular/http";
 
 @Component({
   selector: 'app-contact-us',
@@ -7,8 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  name: string;
+  message: string;
+  subject: string;
+  endpoint: string;
 
-  ngOnInit(): void {
+  http: HttpClient;
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  ngOnInit() {
+    //This data could really come from some inputs on the interface - but let's keep it simple.
+    this.email = " ";
+    this.name = " ";
+    this.subject = " "
+    this.message = " ";
+
+    //Start php via the built in server: $ php -S localhost:8000
+    this.endpoint = "http://boonbox.shop/assets/sendEmail.php";
+  }
+
+  sendEmail() {
+    console.log(
+      this.email,
+    this.name,
+    this.subject,
+    this.message,
+    );
+    
+    // let postVars = {
+    //   email: this.email,
+    //   name: this.name,
+    //   subject: this.subject,
+    //   message: this.message
+    // };
+
+    // //You may also want to check the response. But again, let's keep it simple.
+    // this.http.post(this.endpoint, postVars)
+    //   .subscribe(
+    //     response => console.log(response),
+    //     response => console.log(response)
+    //   )
   }
 }
